adeab55da054143b3f901fac726f16aad93020a2
reallybadchess <m.raphael150@gmail.com>
2026-07-26 21:13:04 -0600

    First critical thinking project completed

---
+++ ct_1/src/ct_1/Individual.java
@@ -0,0 +1,27 @@
+package ct_1;
+
+public class Individual {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName;
+		String lastName;
+		String streetAddress;
+		String city;
+		String zipCode; // string because not used for calculations
+		
+		firstName = "Magdalenian";
+		lastName = "Girl";
+		streetAddress = "13000 Cap Blanc Rock";
+		city = "Dordogne";
+		zipCode = "24560";
+		
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(streetAddress);
+		System.out.println(city);
+		System.out.println(zipCode);
+		
+	}
+
+}
